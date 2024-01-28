269dcb9ee9258f89c6bcf31ef25cf00dee0e05b4
gigabyteTheGreat <157996807+gigabyteTheGreat@users.noreply.github.com>
2024-01-28 14:48:11 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let audio = new Audio('but hopefully fixed_A02.mp3');
+
+function show() {
+    document.getElementById('image')
+    .style.display = "block";
+document.getElementById('btnID')
+    .style.display = "none"
+    audio.play();
+}
